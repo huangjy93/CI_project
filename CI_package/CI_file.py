5eb9e207379687014e20e9b1e11a763ea69808d1
huangjy93 <315135208@qq.com>
2019-01-14 21:25:34 +0800

    提交信息~~~

---
+++ CI_package/CI_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+from selenium import webdriver      #从selenium导入webdriver
+driver=webdriver.Firefox()
+driver.get('http://www.baidu.com')
+
+#by_id
+driver.find_element_by_id("kw").send_keys("selenium")
+time.sleep(3)
+driver.find_element_by_id('su').click()
+time.sleep(5)
+driver.quit()
